9b82205f4b3262604506a9103110fd6a72c854ec
abbybaldauf <90356066+abbybaldauf@users.noreply.github.com>
2021-11-17 05:42:05 -0500

    Update server.py

--- server.py
+++ server.py
@@ -744,7 +744,7 @@ def addFriend():
                 
     elif len(activity) == 0 and len(size) == 0 and len(build) == 0 and len(city) == 0 and len(state) == 0:
         try:
-            cursor = g.conn.execute('SELECT DISTINCT D.username, D.name, D.bio, D.profile_pic FROM Dogs_Owned_By_Has_Physique D')
+            cursor = g.conn.execute('SELECT DISTINCT D.username, D.name, D.bio, D.profile_picture FROM Dogs_Owned_By_Has_Physique D')
             for result in cursor:
                 users_two.append(result['username'])
                 names.append(result['name'])
