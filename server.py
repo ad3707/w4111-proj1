745224a38c0156cb2b8e93724bd768e8c191e406
abbybaldauf <90356066+abbybaldauf@users.noreply.github.com>
2021-11-17 01:00:55 -0500

    Update server.py

--- server.py
+++ server.py
@@ -369,174 +369,170 @@ def addFriend():
     profile_pictures = []
     
         
-        if len(username_two) != 0:
-            try:
-                cursor = g.conn.execute('SELECT D.username, D.name, D.bio, D.profile_picture FROM Dogs_Owned_By_Has_Physique D WHERE D.username = (%s)', username_two)
-                for result in cursor:
+    if len(username_two) != 0:
+        try:
+            cursor = g.conn.execute('SELECT D.username, D.name, D.bio, D.profile_picture FROM Dogs_Owned_By_Has_Physique D WHERE D.username = (%s)', username_two)
+            for result in cursor:
                     users_two.append(result['username'])
                     names.append(result['name'])
                     bios.append(result['bio'])
                     profile_pictures.append(result['profile_picture'])
-                cursor.close()
+            cursor.close()
                 
-                context_users_two = dict(data_one = users_two)
-                context_names = dict(data_two = names)
-                context_profile_pictures = dict(data_three = profile_pictures)
-                context_bios = dict(data_four = bios)
-                
-            except Exception:
-                error = 'Search query failed'
-                
-        elif len(city) != 0 and len(state) != 0 and len(size) != 0 and len(build) != 0 and len(activity) != 0:
-            try:
-                cursor = g.conn.execute('SELECT DISTINCT D.username, D.name, D.bio, D.profile_picture FROM Dogs_Owned_By_Has_Physique D, Likes_Activity A, Resides_In R, Address AD, Activities AC WHERE AD.city = (%s) AND AD.state = (%s) AND AD.zip = R.zip AND AD.street_address = R.street_address AND D.username = R.username AND AC.description = (%s) AND AC.activity_id = A.activity_id AND A.username = D.username AND A.name = D.name AND D.size = (%s) AND D.build = (%s)', city, state, activity, size, build)
-                
-                for result in cursor:
-                    users_two.append(result['username'])
-                    names.append(result['name'])
-                    bios.append(result['bio'])
-                    profile_pictures.append(result['profile_picture'])
-                cursor.close()
-                
-                context_users_two = dict(data_one = users_two)
-                context_names = dict(data_two = names)
-                context_profile_pictures = dict(data_three = profile_pictures)
-                context_bios = dict(data_four = bios)
-                
-            except Exception:
-                error = 'Search query failed'
-                
-        elif len(city) != 0 and len(state) != 0 and len(size) != 0 and len(build) != 0:
-            try:
-                cursor = g.conn.execute('SELECT DISTINCT D.username, D.name, D.bio, D.profile_picture FROM Dogs_Owned_By_Has_Physique D, Resides_In R, Address AD WHERE AD.city = (%s) AND AD.state = (%s) AND AD.zip = R.zip AND AD.street_address = R.street_address AND D.username = R.username AND D.size = (%s) AND D.build = (%s)', city, state, size, build)
-                
-                for result in cursor:
-                    users_two.append(result['username'])
-                    names.append(result['name'])
-                    bios.append(result['bio'])
-                    profile_pictures.append(result['profile_picture'])
-                cursor.close()
-                
-                context_users_two = dict(data_one = users_two)
-                context_names = dict(data_two = names)
-                context_profile_pictures = dict(data_three = profile_pictures)
-                context_bios = dict(data_four = bios)
-                
-            except Exception:
-                error = 'Search query failed'
-                
-                
-        elif len(city) != 0 and len(state) != 0 and len(activity) != 0:
-            try: 
-                cursor = g.conn.execute('SELECT DISTINCT D.username, D.name, D.bio, D.profile_picture FROM Dogs_Owned_By_Has_Physique D, Likes_Activity A, Resides_In R, Address AD, Activities AC WHERE AD.city = (%s) AND AD.state = (%s) AND AD.zip = R.zip AND AD.street_address = R.street_address AND D.username = R.username AND AC.description = (%s) AND AC.activity_id = A.activity_id AND A.username = D.username AND A.name = D.name', city, state, activity)
-                for result in cursor:
-                    users_two.append(result['username'])
-                    names.append(result['name'])
-                    bios.append(result['bio'])
-                    profile_pictures.append(result['profile_picture'])
-                cursor.close()
-                
-                context_users_two = dict(data_one = users_two)
-                context_names = dict(data_two = names)
-                context_profile_pictures = dict(data_three = profile_pictures)
-                context_bios = dict(data_four = bios)
-                
-            except Exception:
-                error = 'Search query failed'
-                
-                
-        elif len(size) != 0 and len(build) != 0 and len(activity) != 0:
-            try:
-                cursor = g.conn.execute('SELECT DISTINCT D.username, D.name, D.bio, D.profile_picture FROM Dogs_Owned_By_Has_Physique D, Likes_Activity A, Activities AC WHERE AC.description = (%s) AND AC.activity_id = A.activity_id AND A.username = D.username AND A.name = D.name AND D.size = (%s) AND D.build = (%s)', activity, size, build)
-            
-                for result in cursor:
-                    users_two.append(result['username'])
-                    names.append(result['name'])
-                    bios.append(result['bio'])
-                    profile_pictures.append(result['profile_picture'])
-                cursor.close()
-                
-                context_users_two = dict(data_one = users_two)
-                context_names = dict(data_two = names)
-                context_profile_pictures = dict(data_three = profile_pictures)
-                context_bios = dict(data_four = bios)
-                
-            except Exception:
-                error = 'Search query failed'
-                
-                
-        elif len(city) != 0 and len(state) != 0:
-            try:
-                cursor = g.conn.execute('SELECT DISTINCT D.username, D.name, D.bio, D.profile_picture FROM Dogs_Owned_By_Has_Physique D, Resides_In R, Address AD WHERE AD.city = (%s) AND AD.state = (%s) AND AD.zip = R.zip AND AD.street_address = R.street_address AND D.username = R.username', city, state)
-            
-                for result in cursor:
-                    users_two.append(result['username'])
-                    names.append(result['name'])
-                    bios.append(result['bio'])
-                    profile_pictures.append(result['profile_picture'])
-                cursor.close()
-                
-                context_users_two = dict(data_one = users_two)
-                context_names = dict(data_two = names)
-                context_profile_pictures = dict(data_three = profile_pictures)
-                context_bios = dict(data_four = bios)
-                
-            except Exception:
-                error = 'Search query failed'
-    
-                
-                
-        elif len(size) != 0 and len(build) != 0:
-            try:
-                cursor = g.conn.execute('SELECT DISTINCT D.username, D.name, D.bio, D.profile_picture FROM Dogs_Owned_By_Has_Physique D WHERE D.size = (%s) AND D.build = (%s)', size, build)
-            
-                for result in cursor:
-                    users_two.append(result['username'])
-                    names.append(result['name'])
-                    bios.append(result['bio'])
-                    profile_pictures.append(result['profile_picture'])
-                cursor.close()
-                
-                context_users_two = dict(data_one = users_two)
-                context_names = dict(data_two = names)
-                context_profile_pictures = dict(data_three = profile_pictures)
-                context_bios = dict(data_four = bios)
-                
-            except Exception:
-                error = 'Search query failed'
-                
-                
-        elif len(activity) != 0:
-            try:
-                cursor = g.conn.execute('SELECT DISTINCT D.username, D.name, D.bio, D.profile_picture FROM Dogs_Owned_By_Has_Physique D, Likes_Activity A, Activities AC WHERE AC.description = (%s) AND AC.activity_id = A.activity_id AND A.username = D.username AND A.name = D.name', activity)
-            
-                for result in cursor:
-                    users_two.append(result['username'])
-                    names.append(result['name'])
-                    bios.append(result['bio'])
-                    profile_pictures.append(result['profile_picture'])
-                cursor.close()
-                
-                context_users_two = dict(data_one = users_two)
-                context_names = dict(data_two = names)
-                context_profile_pictures = dict(data_three = profile_pictures)
-                context_bios = dict(data_four = bios)
-                
-            except Exception:
-                error = 'Search query failed'
-                
-        else:
-            users_two.append('HELLO')
-            print(users_two)
             context_users_two = dict(data_one = users_two)
             context_names = dict(data_two = names)
             context_profile_pictures = dict(data_three = profile_pictures)
             context_bios = dict(data_four = bios)
+        except Exception:
+            error = 'Search query failed'
+                
+    elif len(city) != 0 and len(state) != 0 and len(size) != 0 and len(build) != 0 and len(activity) != 0:
+        try:
+            cursor = g.conn.execute('SELECT DISTINCT D.username, D.name, D.bio, D.profile_picture FROM Dogs_Owned_By_Has_Physique D, Likes_Activity A, Resides_In R, Address AD, Activities AC WHERE AD.city = (%s) AND AD.state = (%s) AND AD.zip = R.zip AND AD.street_address = R.street_address AND D.username = R.username AND AC.description = (%s) AND AC.activity_id = A.activity_id AND A.username = D.username AND A.name = D.name AND D.size = (%s) AND D.build = (%s)', city, state, activity, size, build)
+                
+            for result in cursor:
+                users_two.append(result['username'])
+                names.append(result['name'])
+                bios.append(result['bio'])
+                profile_pictures.append(result['profile_picture'])
+            cursor.close()
+                
+            context_users_two = dict(data_one = users_two)
+            context_names = dict(data_two = names)
+            context_profile_pictures = dict(data_three = profile_pictures)
+            context_bios = dict(data_four = bios)
+                
+        except Exception:
+            error = 'Search query failed'
+                
+    elif len(city) != 0 and len(state) != 0 and len(size) != 0 and len(build) != 0:
+        try:
+            cursor = g.conn.execute('SELECT DISTINCT D.username, D.name, D.bio, D.profile_picture FROM Dogs_Owned_By_Has_Physique D, Resides_In R, Address AD WHERE AD.city = (%s) AND AD.state = (%s) AND AD.zip = R.zip AND AD.street_address = R.street_address AND D.username = R.username AND D.size = (%s) AND D.build = (%s)', city, state, size, build)
+                
+            for result in cursor:
+                users_two.append(result['username'])
+                names.append(result['name'])
+                bios.append(result['bio'])
+                profile_pictures.append(result['profile_picture'])
+            cursor.close()
+                
+            context_users_two = dict(data_one = users_two)
+            context_names = dict(data_two = names)
+            context_profile_pictures = dict(data_three = profile_pictures)
+            context_bios = dict(data_four = bios)
+                
+        except Exception:
+            error = 'Search query failed'
+                
+                
+    elif len(city) != 0 and len(state) != 0 and len(activity) != 0:
+        try: 
+            cursor = g.conn.execute('SELECT DISTINCT D.username, D.name, D.bio, D.profile_picture FROM Dogs_Owned_By_Has_Physique D, Likes_Activity A, Resides_In R, Address AD, Activities AC WHERE AD.city = (%s) AND AD.state = (%s) AND AD.zip = R.zip AND AD.street_address = R.street_address AND D.username = R.username AND AC.description = (%s) AND AC.activity_id = A.activity_id AND A.username = D.username AND A.name = D.name', city, state, activity)
+            for result in cursor:
+                users_two.append(result['username'])
+                names.append(result['name'])
+                bios.append(result['bio'])
+                profile_pictures.append(result['profile_picture'])
+            cursor.close()
+                
+            context_users_two = dict(data_one = users_two)
+            context_names = dict(data_two = names)
+            context_profile_pictures = dict(data_three = profile_pictures)
+            context_bios = dict(data_four = bios)
+                
+        except Exception:
+            error = 'Search query failed'
+                
+                
+    elif len(size) != 0 and len(build) != 0 and len(activity) != 0:
+        try:
+            cursor = g.conn.execute('SELECT DISTINCT D.username, D.name, D.bio, D.profile_picture FROM Dogs_Owned_By_Has_Physique D, Likes_Activity A, Activities AC WHERE AC.description = (%s) AND AC.activity_id = A.activity_id AND A.username = D.username AND A.name = D.name AND D.size = (%s) AND D.build = (%s)', activity, size, build)
             
-            error = 'Invalid Search. Search by city and state, or by size and build, or by activity'
+            for result in cursor:
+                users_two.append(result['username'])
+                names.append(result['name'])
+                bios.append(result['bio'])
+                profile_pictures.append(result['profile_picture'])
+            cursor.close()
+                
+            context_users_two = dict(data_one = users_two)
+            context_names = dict(data_two = names)
+            context_profile_pictures = dict(data_three = profile_pictures)
+            context_bios = dict(data_four = bios)
+                
+        except Exception:
+            error = 'Search query failed'
+                
+                
+    elif len(city) != 0 and len(state) != 0:
+        try:
+            cursor = g.conn.execute('SELECT DISTINCT D.username, D.name, D.bio, D.profile_picture FROM Dogs_Owned_By_Has_Physique D, Resides_In R, Address AD WHERE AD.city = (%s) AND AD.state = (%s) AND AD.zip = R.zip AND AD.street_address = R.street_address AND D.username = R.username', city, state)
             
+            for result in cursor:
+                users_two.append(result['username'])
+                names.append(result['name'])
+                bios.append(result['bio'])
+                profile_pictures.append(result['profile_picture'])
+            cursor.close()
+                
+            context_users_two = dict(data_one = users_two)
+            context_names = dict(data_two = names)
+            context_profile_pictures = dict(data_three = profile_pictures)
+            context_bios = dict(data_four = bios)
+                
+        except Exception:
+            error = 'Search query failed'
+    
+                
+                
+    elif len(size) != 0 and len(build) != 0:
+        try:
+            cursor = g.conn.execute('SELECT DISTINCT D.username, D.name, D.bio, D.profile_picture FROM Dogs_Owned_By_Has_Physique D WHERE D.size = (%s) AND D.build = (%s)', size, build)
             
-        return render_template("addFriend.html", error = error, user = username, **context_users_two, **context_names, ** context_profile_pictures, **context_bios)        
+            for result in cursor:
+                users_two.append(result['username'])
+                names.append(result['name'])
+                bios.append(result['bio'])
+                profile_pictures.append(result['profile_picture'])
+            cursor.close()
+                
+            context_users_two = dict(data_one = users_two)
+            context_names = dict(data_two = names)
+            context_profile_pictures = dict(data_three = profile_pictures)
+            context_bios = dict(data_four = bios)
+                
+        except Exception:
+            error = 'Search query failed'
+                
+                
+    elif len(activity) != 0:
+        try:
+            cursor = g.conn.execute('SELECT DISTINCT D.username, D.name, D.bio, D.profile_picture FROM Dogs_Owned_By_Has_Physique D, Likes_Activity A, Activities AC WHERE AC.description = (%s) AND AC.activity_id = A.activity_id AND A.username = D.username AND A.name = D.name', activity)
+            
+            for result in cursor:
+                users_two.append(result['username'])
+                names.append(result['name'])
+                bios.append(result['bio'])
+                profile_pictures.append(result['profile_picture'])
+            cursor.close()
+                
+            context_users_two = dict(data_one = users_two)
+            context_names = dict(data_two = names)
+            context_profile_pictures = dict(data_three = profile_pictures)
+            context_bios = dict(data_four = bios)
+                
+        except Exception:
+            error = 'Search query failed'
+                
+    else:
+        users_two.append('HELLO')
+        print(users_two)
+        context_users_two = dict(data_one = users_two)
+        context_names = dict(data_two = names)
+        context_bios = dict(data_four = bios)
+        error = 'Invalid Search. Search by city and state, or by size and build, or by activity'
+        
+    return render_template("addFriend.html", error = error, user = username, **context_users_two, **context_names, ** context_profile_pictures, **context_bios)        
 
 @app.route('/addDog', methods = ["GET", "POST"])
 def addDog():
